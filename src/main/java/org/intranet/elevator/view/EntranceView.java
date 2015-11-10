16dbfea1be00082caee6bda618cec9924f2f752f
Wouter1 <W.Pasman@TUDelft.nl>
2015-11-10 14:34:07 +0100

    #2850 elevator up and down lights double size

--- src/main/java/org/intranet/elevator/view/EntranceView.java
+++ src/main/java/org/intranet/elevator/view/EntranceView.java
@@ -1,7 +1,7 @@
 /*
-* Copyright 2003 Neil McKellar and Chris Dailey
-* All rights reserved.
-*/
+ * Copyright 2003 Neil McKellar and Chris Dailey
+ * All rights reserved.
+ */
 package org.intranet.elevator.view;
 
 import java.awt.Color;
@@ -12,53 +12,62 @@ import javax.swing.JComponent;
 import org.intranet.elevator.model.CarEntrance;
 
 /**
-* @author Neil McKellar and Chris Dailey
-*/
-public class EntranceView
-  extends JComponent
-{
-  private CarEntrance entrance;
-  void initialize(CarEntrance entrance, int width, int height)
-  {
-    this.entrance = entrance;
-    setSize(width, height);
-    setOpaque(false);
-  }
+ * @author Neil McKellar and Chris Dailey
+ */
+public class EntranceView extends JComponent {
+	private CarEntrance entrance;
 
-  EntranceView()
-  {
-    super();
-  }
+	/**
+	 * 
+	 * @param entrance
+	 *            the {@link CarEntrance} (the model)
+	 * @param width
+	 *            the entire width including buttons
+	 * @param height
+	 *            the entire height including buttons.
+	 */
+	void initialize(CarEntrance entrance, int width, int height) {
+		this.entrance = entrance;
+		setSize(width, height);
+		setOpaque(false);
+	}
 
-  public void paintComponent(Graphics g)
-  {
-    int marginX = (int)(getWidth()*0.10);
-    int marginY = (int)(getHeight()*0.10);
-    int entranceWidth = getWidth() - (marginX * 2);
-    int entranceHeight = getHeight() - (marginY * 2);
-    int pctClosed = entrance.getDoor().getPercentClosed();
-    int doorWidth = entranceWidth * pctClosed / 200; 
-    
-    // Draw door entrance border
-    g.setColor(Color.white);
-    g.drawRect(marginX, marginY, entranceWidth, entranceHeight);
+	EntranceView() {
+		super();
+	}
 
-    // Draw elevator doors
-    g.setColor(Color.black);
-    g.fillRect(marginX, marginY, doorWidth, entranceHeight);
-    g.fillRect(marginX+entranceWidth-doorWidth, marginY,
-               doorWidth, entranceHeight);
-    
-    // Up indicator next to elevator door
-    if (entrance.isUp()) g.setColor(Color.green);
-    else g.setColor(Color.gray);          
-    g.fillRect(0, 0, marginX, marginY/2);
+	public void paintComponent(Graphics g) {
+		int marginX = (int) (getWidth() * 0.10);
+		int marginY = (int) (getHeight() * 0.10);
+		int entranceWidth = getWidth() - (marginX * 2);
+		int entranceHeight = getHeight() - (marginY * 2);
+		int pctClosed = entrance.getDoor().getPercentClosed();
+		int doorWidth = entranceWidth * pctClosed / 200;
 
-    // Down indicator next to elevator door
-    if (entrance.isDown()) g.setColor(Color.green);
-    else g.setColor(Color.gray);          
-    g.fillRect(0, marginY/2, marginX, marginY/2);
+		// Draw door entrance border
+		g.setColor(Color.white);
+		g.drawRect(marginX, marginY, entranceWidth, entranceHeight);
 
-    g.setColor(Color.black);
-  }
+		// Draw elevator doors
+		g.setColor(Color.black);
+		g.fillRect(marginX, marginY, doorWidth, entranceHeight);
+		g.fillRect(marginX + entranceWidth - doorWidth, marginY, doorWidth,
+				entranceHeight);
+
+		// Up indicator next to elevator door
+		if (entrance.isUp())
+			g.setColor(Color.green);
+		else
+			g.setColor(Color.gray);
+		g.fillRect(0, 0, marginX, marginY);
+
+		// Down indicator next to elevator door
+		if (entrance.isDown())
+			g.setColor(Color.green);
+		else
+			g.setColor(Color.gray);
+		g.fillRect(0, marginY, marginX, marginY);
+
+		g.setColor(Color.black);
+	}
 }
