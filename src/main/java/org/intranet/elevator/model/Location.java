b8af2cda0506986130d7014285b91f4e6bc93ea3
Wouter1 <W.Pasman@TUDelft.nl>
2015-11-11 08:46:01 +0100

    #3738 layout, doc

--- src/main/java/org/intranet/elevator/model/Location.java
+++ src/main/java/org/intranet/elevator/model/Location.java
@@ -1,70 +1,63 @@
 /*
-* Copyright 2003-2005 Neil McKellar and Chris Dailey
-* All rights reserved.
-*/
+ * Copyright 2003-2005 Neil McKellar and Chris Dailey
+ * All rights reserved.
+ */
 package org.intranet.elevator.model;
 
-import java.util.List;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.intranet.sim.ModelElement;
 import org.intranet.sim.event.EventQueue;
 
 /**
-* @author Neil McKellar and Chris Dailey
-*/
-public class Location
-  extends ModelElement
-{
-  private float height;
-  private int capacity;
-  private List people = new ArrayList();
-  
-  Location(EventQueue eQ, float height, int capacity)
-  {
-    super(eQ);
-    this.height = height;
-    this.capacity = capacity;
-  }
+ * @author Neil McKellar and Chris Dailey
+ */
+public class Location extends ModelElement {
+	private float height;
+	private int capacity;
+	private List people = new ArrayList();
 
-  public final float getHeight()
-  {
-    return height;
-  }
+	Location(EventQueue eQ, float height, int capacity) {
+		super(eQ);
+		this.height = height;
+		this.capacity = capacity;
+	}
 
-  protected void setHeight(float newHeight)
-  {
-    height = newHeight; 
-  }
-  
-  public final void personEnters(Object person)
-  {
-    if (isAtCapacity())
-      throw new IllegalStateException("Location is at capacity: " + capacity);
-    people.add(person);
-  }
-  
-  public final Iterator getPeople()
-  {
-    return people.iterator();
-  }
-  
-  public final void personLeaves(Object person)
-  {
-    if (!people.remove(person))
-    {
-      throw new IllegalStateException("Person is not in this location.");
-    }
-  }
-  
-  public final boolean isAtCapacity()
-  {
-    return (people.size() == capacity);
-  }
-  
-  public final int getCapacity()
-  {
-    return capacity;
-  }
+	/**
+	 * @return Current position of the elevator.
+	 */
+	public final float getHeight() {
+		return height;
+	}
+
+	protected void setHeight(float newHeight) {
+		height = newHeight;
+	}
+
+	public final void personEnters(Object person) {
+		if (isAtCapacity())
+			throw new IllegalStateException("Location is at capacity: "
+					+ capacity);
+		people.add(person);
+	}
+
+	public final Iterator getPeople() {
+		return people.iterator();
+	}
+
+	public final void personLeaves(Object person) {
+		if (!people.remove(person)) {
+			throw new IllegalStateException("Person is not in this location.");
+		}
+	}
+
+	public final boolean isAtCapacity() {
+		return (people.size() == capacity);
+	}
+
+	public final int getCapacity() {
+		return capacity;
+	}
 }
